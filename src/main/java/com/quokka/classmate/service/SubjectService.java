701bf10f90cbab4c9957f3c951a3bcc04f4d04f8
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-29 22:13:09 +0900

    type : feat
    과목 조회 관련 서비스 비즈니스 로직 작성

--- src/main/java/com/quokka/classmate/service/SubjectService.java
+++ src/main/java/com/quokka/classmate/service/SubjectService.java
@@ -1,8 +1,13 @@
 package com.quokka.classmate.service;
 
+import com.quokka.classmate.domain.dto.SubjectResponseDto;
+import com.quokka.classmate.domain.entity.Subject;
 import com.quokka.classmate.repository.SubjectRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -10,5 +15,18 @@ public class SubjectService {
 
     private final SubjectRepository subjectRepository;
 
+    // 모든 과목 정보들 조회(과목 리스트 메인 페이지 접속)
+    @Transactional(readOnly = true)
+    public List<SubjectResponseDto> findAll() {
+        return subjectRepository.findAll().stream().map(SubjectResponseDto::new).toList();
+    }
 
+    // 과목 검색 기능
+    @Transactional(readOnly = true)
+    public SubjectResponseDto findById(Long id) throws NullPointerException {
+        Subject subject = subjectRepository.findById(id)
+                .orElseThrow(() -> new NullPointerException("조회하는 과목이 존재하지 않습니다."));
+
+        return new SubjectResponseDto(subject);
+    }
 }
