a7efac03918132b369578d241207ce2877f7ca76
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-29 23:27:14 +0900

    type : test
    테스트 실행 이전 Mock 데이터 담기 프레임 생성

--- src/test/java/com/quokka/classmate/service/SubjectServiceTest.java
+++ src/test/java/com/quokka/classmate/service/SubjectServiceTest.java
@@ -1,12 +1,18 @@
 package com.quokka.classmate.service;
 
+import com.quokka.classmate.domain.entity.Subject;
 import com.quokka.classmate.repository.SubjectRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,6 +24,18 @@ class SubjectServiceTest {
 
     @InjectMocks // Mock 객체 레포지토리 주입
     private SubjectService subjectService;
+
+    @BeforeAll
+    static void saveMockData() {
+        // Mock 데이터 담기용 리스트 생성
+        List<Subject> mockSubjects = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            Subject subject = new Subject();
+
+            
+        }
+    }
 
     @Test
     @DisplayName("강의 과목 전체 조회 시, 누락되는 과목이 없어야 한다.")
