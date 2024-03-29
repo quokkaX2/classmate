77c20356fae8f0d209e0fa6340594604a676fcbd
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-29 20:08:16 +0900

    initial setting

---
+++ src/main/java/com/quokka/classmate/ClassmateApplication.java
@@ -0,0 +1,13 @@
+package com.quokka.classmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassmateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassmateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quokka/classmate/ClassmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quokka.classmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassmateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
