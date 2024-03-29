fa3120fc27fbf24ab726eb69eeaef61d9a57657b
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-29 23:32:20 +0900

    type : feat
    엔티티 비즈니스 로직 및 생성자 작성

--- src/main/java/com/quokka/classmate/domain/entity/Subject.java
+++ src/main/java/com/quokka/classmate/domain/entity/Subject.java
@@ -1,6 +1,7 @@
 package com.quokka.classmate.domain.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -25,4 +26,20 @@ public class Subject {
 
     @Column(nullable = false, name = "credit")
     private Integer credit;
+
+    public Subject(String name, Integer limitCount, Integer time, Integer credit) {
+        this.name = name;
+        this.limitCount = limitCount;
+        this.time = time;
+        this.credit = credit;
+    }
+
+    // 제한인원 카운팅 엔티티 로직
+    public void cutCount() throws IllegalArgumentException {
+        if (this.limitCount == 0) {
+            throw new IllegalArgumentException("수강 인원이 다 찼습니다");
+        }
+
+        this.limitCount--;
+    }
 }
