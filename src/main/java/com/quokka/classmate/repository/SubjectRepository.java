79016712b6c189afcc79b22e3ad1364c77ce6312
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-30 01:23:15 +0900

    type : feat
    과목 키워드 검색 레포지토리 메소드 작성

--- src/main/java/com/quokka/classmate/repository/SubjectRepository.java
+++ src/main/java/com/quokka/classmate/repository/SubjectRepository.java
@@ -4,6 +4,12 @@ import com.quokka.classmate.domain.entity.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
+    // 검색 기반 강의 조회
+    List<Subject> findByNameContaining(String input);
 }
