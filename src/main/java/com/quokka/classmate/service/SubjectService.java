04b1c8665788cba0943fee4cde38dd47727f4130
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-30 16:20:53 +0900

    refactor: 어노테이션 중복 수정

--- src/main/java/com/quokka/classmate/service/SubjectService.java
+++ src/main/java/com/quokka/classmate/service/SubjectService.java
@@ -11,18 +11,17 @@ import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Transactional(readOnly = true)
 public class SubjectService {
 
     private final SubjectRepository subjectRepository;
 
     // 모든 과목 정보들 조회(과목 리스트 메인 페이지 접속)
-    @Transactional(readOnly = true)
     public List<SubjectResponseDto> findAll() {
         return subjectRepository.findAll().stream().map(SubjectResponseDto::new).toList();
     }
 
     // 과목 키워드 검색
-    @Transactional(readOnly = true)
     public List<SubjectResponseDto> findByKeyword(String input) {
         return subjectRepository.findByNameContaining(input)
                 .stream().map(SubjectResponseDto::new).toList();
