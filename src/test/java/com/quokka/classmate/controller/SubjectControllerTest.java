880a274f1c62dd42a8a43c4f837e9053a7239af2
kimD0ngjun <chickentasty0112@gmail.com>
2024-03-31 22:16:28 +0900

    test: 컨트롤러 테스트 코드 작성 종료

--- src/test/java/com/quokka/classmate/controller/SubjectControllerTest.java
+++ src/test/java/com/quokka/classmate/controller/SubjectControllerTest.java
@@ -4,6 +4,7 @@ import com.quokka.classmate.domain.dto.SubjectResponseDto;
 import com.quokka.classmate.domain.entity.Subject;
 import com.quokka.classmate.service.SubjectService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -11,11 +12,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -49,12 +52,12 @@ class SubjectControllerTest {
 
     @Test
     @WithMockUser
+    @DisplayName("/ 엔드포인트로 진입하면 과목 DTO 리스트가 모델에 할당돼서 뷰로 전달진다.")
     void getAllSubjects() throws Exception {
-        // when
+        // when, then
         // mock 서비스 메소드 반환타입 지정
         when(subjectService.findAll()).thenReturn(mockSubjectResponses);
 
-        // then
         // GET 요청 및 뷰 및 모델 검증
         mockMvc.perform(get("/")) // GET 요청 수행
                 .andExpect(status().isOk()) // 상태 검증
@@ -64,6 +67,19 @@ class SubjectControllerTest {
     }
 
     @Test
-    void getSubjectByInput() {
+    @WithMockUser
+    @DisplayName("/api/search 엔드포인트로 진입하면 쿼리 매개값 기반 과목 DTO 리스트가 모델에 할당돼서 뷰로 전달진다.")
+    void getSubjectByInput() throws Exception {
+        // when, then
+        // mock 서비스 메소드 반환타입 지정
+        when(subjectService.findByKeyword(anyString())).thenReturn(mockSubjectResponses);
+
+        // GET 요청 및 뷰 및 모델 검증
+        ResultActions resultActions =
+                mockMvc.perform(get("/api/search").param("input", "검색"))
+                .andExpect(status().isOk()) // 상태 검증
+                .andExpect(view().name("main"))
+                .andExpect(model().attributeExists("subjects"))
+                .andExpect(model().attribute("subjects", mockSubjectResponses));
     }
 }
