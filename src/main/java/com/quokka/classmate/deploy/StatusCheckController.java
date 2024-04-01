95aa974872316dfce32464cec34b854c8be5c674
kimD0ngjun <chickentasty0112@gmail.com>
2024-04-01 22:45:28 +0900

    feat: 추가 커밋 - 메소드 비교 필요

--- src/main/java/com/quokka/classmate/deploy/StatusCheckController.java
+++ src/main/java/com/quokka/classmate/deploy/StatusCheckController.java
@@ -10,6 +10,13 @@ public class StatusCheckController {
 
     @GetMapping("/health-check")
     public ResponseEntity<Void> checkHealthStatus() {
+        System.out.println("/health-check 수신 확인");
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<Void> checkHealth() {
+        System.out.println("/health 수신 확인");
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
