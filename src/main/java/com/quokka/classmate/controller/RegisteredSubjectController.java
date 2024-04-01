fcb9e611fffa556662c7675aa081405e3f6a21b9
kimD0ngjun <chickentasty0112@gmail.com>
2024-04-02 02:39:42 +0900

    feat: 장바구니 과목 삭제 컨트롤러 메소드 추가

--- src/main/java/com/quokka/classmate/controller/RegisteredSubjectController.java
+++ src/main/java/com/quokka/classmate/controller/RegisteredSubjectController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,10 +31,20 @@ public class RegisteredSubjectController {
 
     // 수강 과목 장바구니에 추가
     @PostMapping("/api/cart/{subjectId}")
-    public ResponseEntity<String> containCart(
+    public ResponseEntity<String> saveRegisterSubject(
             @PathVariable Long subjectId,
             @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
         return registeredSubjectService.createRegisterSubject(subjectId, userDetails);
+    }
+
+    // 수강 과목 장바구니에서 삭제
+    // html 파일에서 타임리프로 뿌리기 필요
+    @DeleteMapping("/api/cart/{subjectId}")
+    public ResponseEntity<String> deleteRegisterSubject(
+            @PathVariable Long subjectId,
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        return null;
     }
 }
