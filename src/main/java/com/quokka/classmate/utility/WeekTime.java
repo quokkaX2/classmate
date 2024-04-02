f2f4ea17b6ec161a43deb5aaaca6c55cf11e2909
kimD0ngjun <chickentasty0112@gmail.com>
2024-04-02 18:52:54 +0900

    feat: 모듈러 연산 기반 시간 출력 정적 메소드 작성

--- src/main/java/com/quokka/classmate/utility/WeekTime.java
+++ src/main/java/com/quokka/classmate/utility/WeekTime.java
@@ -2,11 +2,25 @@ package com.quokka.classmate.utility;
 
 public class WeekTime {
 
+    // 요일과 시간대를 같이 출력하기 위한 간이 모듈려 연산식 + 배열 인덱스
     public static String calculateWeekTime(Integer time) {
 
-        int day = time / 10; // 요일
-        int classTime = time % 10; // 강의 시간대(강의 시간단위는 1시간)
+        String[] days = {"월", "화", "수", "목", "금"}; // 요일
+        String[] times = {
+                "09:00 ~ 10:00",
+                "10:00 ~ 11:00",
+                "11:00 ~ 12:00",
+                "12:00 ~ 13:00",
+                "13:00 ~ 14:00",
+                "14:00 ~ 15:00",
+                "15:00 ~ 16:00",
+                "16:00 ~ 17:00",
+                "17:00 ~ 18:00"
+        }; // 강의 시간대
 
-        return null;
+        String day = days[time / 10];
+        String classTime = times[time % 10 - 1]; // 인덱스는 0부터 시작하므로
+
+        return day + "요일 " + classTime;
     }
 }
