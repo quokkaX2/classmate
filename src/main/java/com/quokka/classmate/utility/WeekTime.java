60bbc5d59d84d6f592d9a5eb8c648a871b3291e7
kimD0ngjun <chickentasty0112@gmail.com>
2024-04-02 18:26:04 +0900

    feat: 일주일 시간 책정 정적 메소드 프레임

---
+++ src/main/java/com/quokka/classmate/utility/WeekTime.java
@@ -0,0 +1,12 @@
+package com.quokka.classmate.utility;
+
+public class WeekTime {
+
+    public static String calculateWeekTime(Integer time) {
+
+        int day = time / 10; // 요일
+        int classTime = time % 10; // 강의 시간대(강의 시간단위는 1시간)
+
+        return null;
+    }
+}
