a887304bc6a2fe70e4119b4913a202c6ee5da525
kimD0ngjun <chickentasty0112@gmail.com>
2024-04-09 13:40:48 +0900

    fix: JSON 반환 형식으로 컨트롤러 작성

--- src/main/java/com/quokka/classmate/controller/SubjectController.java
+++ src/main/java/com/quokka/classmate/controller/SubjectController.java
@@ -3,14 +3,16 @@ package com.quokka.classmate.controller;
 import com.quokka.classmate.domain.dto.SubjectResponseDto;
 import com.quokka.classmate.service.SubjectService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @AllArgsConstructor
 public class SubjectController {
 
@@ -18,19 +20,19 @@ public class SubjectController {
 
     @GetMapping("/")
     public String getAllSubjects(Model model) {
-        List<SubjectResponseDto> subjects = subjectService.findAll();
-        model.addAttribute("subjects", subjects);
-        model.addAttribute("currentPage", "courses");
+//        List<SubjectResponseDto> subjects = subjectService.findAll();
+//        model.addAttribute("subjects", subjects);
+//        model.addAttribute("currentPage", "courses");
 
         return "main";
     }
 
     @GetMapping("/api/search")
-    public String getSubjectByInput(@RequestParam String input, Model model) {
+    public ResponseEntity<List<SubjectResponseDto>> getSubjectByInput(@RequestParam String input) {
         List<SubjectResponseDto> subjects = subjectService.findByKeyword(input);
-        model.addAttribute("subjects", subjects);
-        model.addAttribute("currentPage", "courses");
+//        model.addAttribute("subjects", subjects);
+//        model.addAttribute("currentPage", "courses");
 
-        return "main";
+        return ResponseEntity.ok().body(subjects);
     }
 }
