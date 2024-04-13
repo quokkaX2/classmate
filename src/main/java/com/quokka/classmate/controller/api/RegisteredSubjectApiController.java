55f0c2b02753f136c78cd6a2866c8c329d27a656
kimD0ngjun <chickentasty0112@gmail.com>
2024-04-13 17:25:30 +0900

    refactor: 주석 해제 및 redisson 기반 변경

--- src/main/java/com/quokka/classmate/controller/api/RegisteredSubjectApiController.java
+++ src/main/java/com/quokka/classmate/controller/api/RegisteredSubjectApiController.java
@@ -1,6 +1,6 @@
 package com.quokka.classmate.controller.api;
 
-//import com.quokka.classmate.facade.RedissonLockFacade;
+import com.quokka.classmate.facade.RedissonLockFacade;
 import com.quokka.classmate.global.exception.ApiResponseDto;
 import com.quokka.classmate.global.security.UserDetailsImpl;
 import com.quokka.classmate.service.RegisteredSubjectService;
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class RegisteredSubjectApiController {
 
     private final RegisteredSubjectService registeredSubjectService;
-//    private final RedissonLockFacade redissonLockFacade;
+    private final RedissonLockFacade redissonLockFacade;
 
     // 수강 과목 장바구니에 추가
     @PostMapping("/api/cart/{subjectId}")
@@ -50,8 +50,8 @@ public class RegisteredSubjectApiController {
             @AuthenticationPrincipal UserDetailsImpl userDetails) {
 
 //        registeredSubjectService.registrationSubject(subjectId, userDetails.getUser().getId());
-        registeredSubjectService.registrationSubjectByPessimisticLock(subjectId, userDetails.getUser().getId());
-//        redissonLockFacade.registerSubject(subjectId, userDetails.getUser().getId());
+//        registeredSubjectService.registrationSubjectByPessimisticLock(subjectId, userDetails.getUser().getId());
+        redissonLockFacade.registerSubject(subjectId, userDetails.getUser().getId());
 
         return ResponseEntity.ok(new ApiResponseDto("수강 신청이 완료되었습니다."));
     }
