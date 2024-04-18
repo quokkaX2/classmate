197725b14bf1fc2854dcce4663f27c437d0b3699
Klarra <klarra247@gmail.com>
2024-04-18 22:14:09 +0800

    refactor: 커서 기반 페이징 네이티브 쿼리 사용

--- src/main/java/com/quokka/classmate/controller/api/SubjectApiController.java
+++ src/main/java/com/quokka/classmate/controller/api/SubjectApiController.java
@@ -14,20 +14,30 @@ public class SubjectApiController {
     private final SubjectService subjectService;
 
 
-    @GetMapping("/api/search")
-    public ResponseEntity<?> getSubjectsByInputAndCursor(@RequestParam String input,
-                                                         @RequestParam(defaultValue = "0") int page,
-                                                         @RequestParam(defaultValue = "10") int size) {
 
-//        List<SubjectResponseDto> subjects = subjectService.findByKeyword(input);
-//        model.addAttribute("subjects", subjects);
-//        model.addAttribute("currentPage", "courses");
+        @GetMapping("/api/search")
+    public ResponseEntity<?> getSubjectsByInputAndCursor(@RequestParam String input,
+                                                         @RequestParam(required = false) Long cursor,
+                                                         @RequestParam(defaultValue = "10") int size) {
         if (input.isEmpty()) {
             throw new IllegalArgumentException("검색어를 입력해주세요.");
         }
-        //return subjectService.searchTitleByIndexing(input, page-1, size);
-//        return subjectService.findByKeyword(input);
-        return ResponseEntity.ok(subjectService.searchByCursorWithoutNative(input, null, page, size));
-
+        return ResponseEntity.ok(subjectService.searchByCursor(input, cursor, size));
     }
+//    @GetMapping("/api/search")
+//    public ResponseEntity<?> getSubjectsByInputAndCursor(@RequestParam String input,
+//                                                         @RequestParam(defaultValue = "0") int page,
+//                                                         @RequestParam(defaultValue = "10") int size) {
+//
+////        List<SubjectResponseDto> subjects = subjectService.findByKeyword(input);
+////        model.addAttribute("subjects", subjects);
+////        model.addAttribute("currentPage", "courses");
+//        if (input.isEmpty()) {
+//            throw new IllegalArgumentException("검색어를 입력해주세요.");
+//        }
+//        //return subjectService.searchTitleByIndexing(input, page-1, size);
+////        return subjectService.findByKeyword(input);
+//        return ResponseEntity.ok(subjectService.searchByCursorWithoutNative(input, null, page, size));
+//
+//    }
 }
