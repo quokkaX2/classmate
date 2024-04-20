693274f1bdeca18e1626af5c8d5eb3a0dda83c1b
qkrckstjq <cksk49@naver.com>
2024-04-20 15:14:41 +0900

    feat: redis queue에 추가하는 addQueue 메소드 추가

--- src/main/java/com/quokka/classmate/service/QueueService.java
+++ src/main/java/com/quokka/classmate/service/QueueService.java
@@ -1,5 +1,9 @@
 package com.quokka.classmate.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.quokka.classmate.domain.dto.RedisQueueRequestDto;
+import com.quokka.classmate.domain.entity.Student;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -9,12 +13,18 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class QueueService {
+    private final String ADD_QUEUE = "대기열 추가";
+    private final String PROCESS = "수강신청 처리";
+    private final ObjectMapper objectMapper;
     private final int FIRST_INDEX = 0;
     private final int LAST_INDEX = -1;
     private final RedisTemplate<String, String> redisTemplate;
 
-    public void addQueue() {
-
+    public void addQueue(RedisQueueRequestDto requestDto) throws JsonProcessingException {
+        String value = objectMapper.writeValueAsString(requestDto);
+        Long score = System.currentTimeMillis();
+        redisTemplate.opsForZSet().add(ADD_QUEUE, value, score);
+        log.info(requestDto.getStudentId() + "번 학생이 " + requestDto.getSubjectId() + "번 과목" + score + "에 신청함");
     }
 
 }
