c5a6e45aea02c61e9b77f6ae61a4f5807f96d203
qkrckstjq <cksk49@naver.com>
2024-04-20 15:05:30 +0900

    feat: redis queue에 저장할 RedisQueueRequestDto 구현

--- src/main/java/com/quokka/classmate/service/QueueService.java
+++ src/main/java/com/quokka/classmate/service/QueueService.java
@@ -1,4 +1,20 @@
 package com.quokka.classmate.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Slf4j(topic = "Redis Queue Service")
+@Service
+@RequiredArgsConstructor
 public class QueueService {
+    private final int FIRST_INDEX = 0;
+    private final int LAST_INDEX = -1;
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public void addQueue() {
+
+    }
 
 }
