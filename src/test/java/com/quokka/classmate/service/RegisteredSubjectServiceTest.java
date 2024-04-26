412fc88e966945c8ab723491eb5bec5b4069eb18
Jun <129869700+kimD0ngjun@users.noreply.github.com>
2024-04-26 18:45:07 +0900

    Revert "ES and Spring Connecting"

--- src/test/java/com/quokka/classmate/service/RegisteredSubjectServiceTest.java
+++ src/test/java/com/quokka/classmate/service/RegisteredSubjectServiceTest.java
@@ -1,18 +1,18 @@
-//package com.quokka.classmate.service;
-//
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//
-//
-//@ExtendWith(MockitoExtension.class) // JUnit 5와 Mockito 연동
-//class RegisteredSubjectServiceTest {
-//
-//    // 서비스 독립성 훼손 리팩토링 필요
-//    // 우선은 테스트 프레임만 작성하고 추후 논의
-//
-//    @Test
-//    void createRegisteredSubject() {
-//    }
-//}
-//
+package com.quokka.classmate.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class) // JUnit 5와 Mockito 연동
+class RegisteredSubjectServiceTest {
+
+    // 서비스 독립성 훼손 리팩토링 필요
+    // 우선은 테스트 프레임만 작성하고 추후 논의
+
+    @Test
+    void createRegisteredSubject() {
+    }
+}
+
