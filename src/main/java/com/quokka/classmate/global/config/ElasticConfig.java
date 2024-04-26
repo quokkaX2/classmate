fdebb4dfbe87add64c8769fb2bb41c0258d34539
kimD0ngjun <chickentasty0112@gmail.com>
2024-04-26 17:09:14 +0900

    fix: 설정 일부 변경

--- src/main/java/com/quokka/classmate/global/config/ElasticConfig.java
+++ src/main/java/com/quokka/classmate/global/config/ElasticConfig.java
@@ -11,12 +11,20 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 public class ElasticConfig extends ElasticsearchConfiguration {
 
 //    @Value("${spring.data.elasticsearch.url}")
-    @Value("${spring.elasticsearch.rest.uris}")
+    @Value("${spring.elasticsearch.uris}")
     private String elasticUrl;
+
+    @Value("{spring.elasticsearch.username")
+    private String username;
+
+    @Value("{spring.elasticsearch.password")
+    private String password;
+
     @Override
     public ClientConfiguration clientConfiguration() {
         return ClientConfiguration.builder()
                 .connectedTo(elasticUrl)
+                .withBasicAuth(username, password)
                 .build();
     }
 }
