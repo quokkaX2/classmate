6a58859cd7d13aa5f38dd56c56f9973fbd6a571d
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-04-30 16:16:23 +0900

    Revert "feat: Feature schelock"

--- src/main/java/com/quokka/classmate/ClassmateApplication.java
+++ src/main/java/com/quokka/classmate/ClassmateApplication.java
@@ -1,6 +1,5 @@
 package com.quokka.classmate;
 
-import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -9,7 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableCaching
 @EnableScheduling
-@EnableSchedulerLock(defaultLockAtMostFor = "30s")
 public class ClassmateApplication {
 
     public static void main(String[] args) {
