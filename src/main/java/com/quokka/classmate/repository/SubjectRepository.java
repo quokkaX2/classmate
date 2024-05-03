a813be5dbfa3ce62a5fb88ac3f642be03ba0195e
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-05-03 16:39:20 +0900

    Revert "Feature pcs replication"

--- src/main/java/com/quokka/classmate/repository/SubjectRepository.java
+++ src/main/java/com/quokka/classmate/repository/SubjectRepository.java
@@ -11,13 +11,13 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
-import org.springframework.transaction.annotation.Transactional;
 
 
 import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface SubjectRepository extends JpaRepository<Subject, Long> {
     // 검색 기반 강의 조회
     List<Subject> findByTitleContaining(String input);
@@ -41,7 +41,6 @@ public interface SubjectRepository extends JpaRepository<Subject, Long> {
 //    List<Subject> findByTitleWithCursor(@Param("input") String input, @Param("cursor") Long cursor, @Param("size") int size);
 
     // FULLTEXT 사용
-    @Transactional(readOnly = true)
     @Query(value = "SELECT * FROM subjects WHERE MATCH(title) AGAINST(:input IN BOOLEAN MODE) AND (:cursor IS NULL OR subject_id > :cursor) ORDER BY subject_id ASC LIMIT :size", nativeQuery = true)
     List<Subject> findByTitleWithCursor(@Param("input") String input, @Param("cursor") Long cursor, @Param("size") int size);
     @Query("SELECT s FROM Subject s WHERE s.title LIKE CONCAT('%', :input, '%') AND (:cursor IS NULL OR s.id > :cursor)")
